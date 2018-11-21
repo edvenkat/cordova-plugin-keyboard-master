366915db001bfc06cb857d2755942cd0ba30e874
Venka Tesh G <gvenkatesh225@gmail.com>
2018-11-21 10:13:52 +0530

    Update CDVKeyboard.m

--- src/ios/CDVKeyboard.m
+++ src/ios/CDVKeyboard.m
@@ -211,12 +211,13 @@ static IMP WKOriginalImp;
     
     // A view's frame is in its superview's coordinate system so we need to convert again
     self.webView.frame = [self.webView.superview convertRect:screen fromView:self.webView];
-  
+  /*
     // I'm sure there's a better way...
     if (@available(iOS 12, *)) {
         CGSize revisedSize = CGSizeMake(self.webView.frame.size.width, self.webView.frame.size.height - keyboard.size.height);
         self.webView.scrollView.contentSize = revisedSize;
     }
+    */
 }
 
 #pragma mark UIScrollViewDelegate
