79cfe38de014c36b4a594afa54499c4ea2dbd4a6
Venka Tesh G <gvenkatesh225@gmail.com>
2019-01-28 17:24:28 +0530

    Update CDVKeyboard.m

--- src/ios/CDVKeyboard.m
+++ src/ios/CDVKeyboard.m
@@ -140,7 +140,7 @@
                                                                  object:nil
                                                                   queue:[NSOperationQueue mainQueue]
                                                              usingBlock:^(NSNotification* notification) {
-                                                                 [weakSelf performSelector:@selector(shrinkViewKeyboardWillChangeFrame:) withObject:notification afterDelay:10];
+                                                                 [weakSelf performSelector:@selector(shrinkViewKeyboardWillChangeFrame:) withObject:notification afterDelay:2];
                                                                  CGRect screen = [[UIScreen mainScreen] bounds];
                                                                  CGRect keyboard = ((NSValue*)notification.userInfo[@"UIKeyboardFrameEndUserInfoKey"]).CGRectValue;
                                                                  CGRect intersection = CGRectIntersection(screen, keyboard);
